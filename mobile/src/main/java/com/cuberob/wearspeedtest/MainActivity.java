2b34ab9eb864f1f49acebd96cc36b133baa207a8
cuberob <robdeknegt@gmail.com>
2015-03-06 16:32:48 +0100

    Very Basic Speedtest

--- mobile/src/main/java/com/cuberob/wearspeedtest/MainActivity.java
+++ mobile/src/main/java/com/cuberob/wearspeedtest/MainActivity.java
@@ -1,68 +1,152 @@
 package com.cuberob.wearspeedtest;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.DataApi;
 import com.google.android.gms.wearable.MessageApi;
 import com.google.android.gms.wearable.MessageEvent;
 import com.google.android.gms.wearable.Node;
 import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.PutDataRequest;
 import com.google.android.gms.wearable.Wearable;
+
+import org.w3c.dom.Text;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Objects;
 
 
-public class MainActivity extends ActionBarActivity implements MessageApi.MessageListener {
+public class MainActivity extends ActionBarActivity implements MessageApi.MessageListener, GoogleApiClient.ConnectionCallbacks {
 
-    private byte[] payload = new byte[1024];
     private GoogleApiClient mGoogleApiClient;
+
+    //Views
+    private Spinner mSpinner;
+    private ProgressDialog dialog;
+    private SeekBar mSeekbar; //TODO: Use to set runs
+    private TextView mLogTextView;
+
+    //Transmission Data
+    private long transmitTime = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mGoogleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
-            @Override
-            public void onConnected(Bundle connectionHint) {
-                Log.d("GoogleApiClient", "onConnected: " + connectionHint);
-            }
-
-            @Override
-            public void onConnectionSuspended(int cause) {
-                Log.d("GoogleApiClient", "onConnectionSuspended: " + cause);
-            }
-        })
+        //Connect to Google Api Client for message api
+        mGoogleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this)
         .addApi(Wearable.API)
         .build();
+
+        setupViews();
+    }
+
+    private void setupViews(){
+        mSeekbar = (SeekBar) findViewById(R.id.seekBar);
+        mLogTextView = (TextView) findViewById(R.id.log_textview);
+        mSpinner = (Spinner) findViewById(R.id.spinner);
+    }
+
+    private int getTestSize(){
+        return getResources().getIntArray(R.array.test_sizes)[mSpinner.getSelectedItemPosition()];
+    }
+
+    /**
+     * Called from xml layout
+     * @param v
+     */
+    public void start(final View v){
+        showLoadingDialog();
+
+        final byte[] payload = new byte[getTestSize()];
+        Log.d("onClick", "Payload size: " + payload.length);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Collection<String> nodes = getNodes();
+                for(String node : nodes){
+                    Log.d("Node", "Sending payload to: " + node);
+                    if(v.getId() == R.id.button) {
+                        sendPayloadAsRequest(payload);
+                    }else{
+                        sendPayloadAsMessage(node, payload);
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private void showLoadingDialog() {
+        dialog = ProgressDialog.show(MainActivity.this, "",
+                "Loading. Please wait...", true);
+        dialog.setCancelable(true);
+    }
+
+    private void sendPayloadAsRequest(byte[] payload) {
+        PutDataMapRequest putDataMapReq = PutDataMapRequest.create("/data");
+        putDataMapReq.getDataMap().putByteArray("payload", payload);
+        putDataMapReq.getDataMap().putLong("time", System.currentTimeMillis()); //If the dataMap hasn't changed it won't get synced
+        PutDataRequest putDataReq = putDataMapReq.asPutDataRequest();
+
+        transmitTime = System.currentTimeMillis();
+
+        PendingResult<DataApi.DataItemResult> pendingResult =
+                Wearable.DataApi.putDataItem(mGoogleApiClient, putDataReq);
+        pendingResult.setResultCallback(new ResultCallback<DataApi.DataItemResult>() {
+            @Override
+            public void onResult(DataApi.DataItemResult dataItemResult) {
+                if(!dataItemResult.getStatus().isSuccess()){
+                    Log.e("putDataItem", "Failed to send message with status code: "
+                            + dataItemResult.getStatus().getStatusCode());
+                    dialog.dismiss();
+                    if(dataItemResult.getStatus().getStatusCode() == 4003){
+                        notifyUser("Payload was to large...");
+                    }else{
+                        notifyUser("Failed to putDataRequest");
+                    }
+                }
+            }
+        });
+    }
+
+    private void notifyUser(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 
 
-    public void start(View v){
-        Log.d("onClick", "Start button");
-        Collection<String> nodes = getNodes();
-        for(String node : nodes){
-            Log.d("Node", "Sending payload to: " + node);
-            sendPayload(node, payload);
-        }
-    }
+    private void sendPayloadAsMessage(String nodeId, byte[] payload) {
+        transmitTime = System.currentTimeMillis();
 
-    private void sendPayload(String nodeId, byte[] payload) {
-        Wearable.MessageApi.sendMessage(
-                mGoogleApiClient, nodeId, "/data", payload).setResultCallback(
+        PendingResult<MessageApi.SendMessageResult> result = Wearable.MessageApi.sendMessage(
+                mGoogleApiClient, nodeId, "/data", payload);
+
+        result.setResultCallback(
                 new ResultCallback<MessageApi.SendMessageResult>() {
                     @Override
                     public void onResult(MessageApi.SendMessageResult sendMessageResult) {
                         if (!sendMessageResult.getStatus().isSuccess()) {
                             Log.e("sendMessage", "Failed to send message with status code: "
                                     + sendMessageResult.getStatus().getStatusCode());
+                            notifyUser("Failed to send message");
+                            dialog.dismiss();
                         }
                     }
                 }
@@ -80,34 +164,15 @@ public class MainActivity extends ActionBarActivity implements MessageApi.Messag
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     protected void onResume() {
         super.onResume();
         if(mGoogleApiClient != null) {
             mGoogleApiClient.connect();
+            Wearable.MessageApi.addListener(mGoogleApiClient, this);
+
         }else{
             Log.e("GoogleApiClient", "mGoogleApiClient null in onResume()!");
+            notifyUser("Could not connect to GoogleApi & Watch");
         }
     }
 
@@ -115,12 +180,36 @@ public class MainActivity extends ActionBarActivity implements MessageApi.Messag
     protected void onPause() {
         super.onPause();
         if(mGoogleApiClient != null){
+            Wearable.MessageApi.removeListener(mGoogleApiClient, this);
             mGoogleApiClient.disconnect();
         }
     }
 
     @Override
     public void onMessageReceived(MessageEvent messageEvent) {
-        Log.d("MessageAPI", "Received message: " + messageEvent.toString());
+        dialog.dismiss();
+
+        String time = new String(messageEvent.getData());
+        Long totalTime = Long.parseLong(time) - transmitTime;
+
+        final String log = "Time: " + totalTime + "ms\n" + mLogTextView.getText().toString();
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mLogTextView.setText(log);
+            }
+        });
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.d("GoogleApiClient", "connected!");
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d("GoogleApiClient", "suspended!");
     }
 }
--- wear/src/main/java/com/cuberob/wearspeedtest/DataLayer.java
+++ wear/src/main/java/com/cuberob/wearspeedtest/DataLayer.java
@@ -1,23 +1,79 @@
 package com.cuberob.wearspeedtest;
 
+import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.MessageApi;
 import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.NodeApi;
+import com.google.android.gms.wearable.Wearable;
 import com.google.android.gms.wearable.WearableListenerService;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by cuberob on 6-3-2015.
  */
-public class DataLayer extends WearableListenerService {
+public class DataLayer extends WearableListenerService{
+
     @Override
     public void onDataChanged(DataEventBuffer dataEvents) {
+        long time = System.currentTimeMillis();
+        Log.d("onMessageReceived", "Received DataEvent");
+        replyTime(time, "");
+    }
 
+    private String getNode(GoogleApiClient mGoogleApiClient) {
+        HashSet<String> results = new HashSet<String>();
+        NodeApi.GetConnectedNodesResult nodes =
+                Wearable.NodeApi.getConnectedNodes(mGoogleApiClient).await();
+        return nodes.getNodes().get(0).getId(); //Assumes single node
     }
 
     @Override
     public void onMessageReceived(MessageEvent messageEvent) {
         super.onMessageReceived(messageEvent);
+        long time = System.currentTimeMillis();
+
         Log.d("onMessageReceived", "Received message: " + messageEvent.toString());
+        replyTime(time, messageEvent.getSourceNodeId());
+    }
+
+    private void replyTime(long time, String nodeId) {
+        GoogleApiClient mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .build();
+
+        mGoogleApiClient.blockingConnect(5, TimeUnit.SECONDS);
+
+        if(!mGoogleApiClient.isConnected()){
+            Log.e("GoogleApiClient", "Failed to connect!");
+            mGoogleApiClient.disconnect(); //Dunno if necessary
+            return;
+        }
+
+        if(nodeId.isEmpty()){
+            nodeId = getNode(mGoogleApiClient);
+        }
+
+        Wearable.MessageApi.sendMessage(
+                mGoogleApiClient, nodeId, "/data", Long.toString(time).getBytes()).setResultCallback(
+                new ResultCallback<MessageApi.SendMessageResult>() {
+                    @Override
+                    public void onResult(MessageApi.SendMessageResult sendMessageResult) {
+                        if (!sendMessageResult.getStatus().isSuccess()) {
+                            Log.e("sendMessage", "Failed to send message with status code: "
+                                    + sendMessageResult.getStatus().getStatusCode());
+                        }
+                    }
+                }
+        );
+
     }
 }
